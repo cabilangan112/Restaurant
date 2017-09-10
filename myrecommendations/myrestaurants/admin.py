874dbf90ee7bd627cf48212081e5af0fd30594aa
cabilangan112 <jassencabilangan@gmail.com>
2017-09-10 15:22:20 +0800

    restaurant

---
+++ myrecommendations/myrestaurants/admin.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+# Register your models here.
+from django.contrib import admin
+import models
+
+admin.site.register(models.Restaurant)
+admin.site.register(models.Dish)
+admin.site.register(models.RestaurantReview)
